fc9e5feee4eedc73e1254049185f82e048516e08
Matt <matthew.defusco@gmail.com>
2023-04-22 13:44:23 -0400

    adding redux toolkit to client

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -11,6 +11,10 @@ module.exports = (app) => {
     res.send("Thanks for your feedback!");
   });
 
+  app.post("/api/surveys/webhooks", (req, res) => {
+    console.log(req.body);
+  });
+
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
